286c93c56eeaef68ec8782c82f51e120868971b0
MrFanCode <ytc26121@gmail.com>
2023-02-05 18:07:52 +0800

    updated

--- main.py
+++ main.py
@@ -1,31 +1,42 @@
 from cryptography.fernet import Fernet 
-import time
 
+# This function will generate encryption key. The generaed key file is required to encrypt or decrypt a file.
 def gen_key():
     key = Fernet.generate_key()
-    with open('crypt.txt', 'wb') as f:
+    with open('key.txt', 'wb') as f:
         f.write(key)
 
 
+# This function will encrypt a file.
 def enc_file():
     filename = str(input("Filename: "))
-    with open(filename, 'rb') as file:
-        content = file.read()
-        with open('crypt.txt', 'rb') as keyfile:
-            crypt = Fernet(keyfile.read())
-            encrypted_content = crypt.encrypt(content)
-            with open(filename, 'wb') as f:
-                f.write(encrypted_content)
+    try:
+        with open(filename, 'rb') as file:
+            content = file.read()
+            with open('key.txt', 'rb') as keyfile:
+                crypt = Fernet(keyfile.read())
+                encrypted_content = crypt.encrypt(content)
+                with open(filename, 'wb') as f:
+                    f.write(encrypted_content)
+                    print("File has been encrypted.")
+    except FileNotFoundError:
+        print("File not found. Please check the file location that you enter.")
 
+
+# This function will decrypt a file.
 def decrypt():
     filename = str(input("Filename: "))
-    with open(filename, 'rb') as file:
-        content = file.read()
-    with open('crypt.txt', 'rb') as keyfile:
-        crypt = Fernet(keyfile.read())
-        decrypted_content = crypt.decrypt(content)
-        with open(filename, 'wb') as f:
-            f.write(decrypted_content)
+    try:
+        with open(filename, 'rb') as file:
+            content = file.read()
+        with open('key.txt', 'rb') as keyfile:
+            crypt = Fernet(keyfile.read())
+            decrypted_content = crypt.decrypt(content)
+            with open(filename, 'wb') as f:
+                f.write(decrypted_content)
+                print("File has been decrypted.")
+    except FileNotFoundError:
+        print("File not found. Please check the file name and the location that uou entered.")
 
 
 def run():
